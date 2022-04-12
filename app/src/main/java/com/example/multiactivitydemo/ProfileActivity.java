e34c8a4fc990a3a6f99c9f52acbe52f7e2f90d34
brom <a97marbr@gmail.com>
2022-04-12 09:55:17 +0200

    Added debug msg to ProfileActivity's lifecycle states.

--- app/src/main/java/com/example/multiactivitydemo/ProfileActivity.java
+++ app/src/main/java/com/example/multiactivitydemo/ProfileActivity.java
@@ -3,12 +3,49 @@ package com.example.multiactivitydemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class ProfileActivity extends AppCompatActivity {
+    private Button signout;
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.d("==>", "ProfileActivity resumed.");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("==>", "ProfileActivity started.");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("==>", "ProfileActivity stopped.");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("==>", "ProfileActivity destroyed.");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        Log.d("==>", "ProfileActivity created.");
+
+        signout = findViewById(R.id.sign_out);
+        signout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("==>", "Sign out clicked.");
+            }
+        });
     }
 }
